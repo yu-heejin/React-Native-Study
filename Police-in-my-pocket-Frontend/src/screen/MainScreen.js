19a3ae2915054c3e7f3b08b16aafbf02f4e7daad
hee <jkgt0521@naver.com>
2022-07-03 01:13:09 +0900

    react native

--- Police-in-my-pocket-Frontend/src/screen/MainScreen.js
+++ Police-in-my-pocket-Frontend/src/screen/MainScreen.js
@@ -1,47 +1,68 @@
 import * as React from 'react';
 import {StyleSheet, Text, View, Image, Button} from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import MyButton from '../component/MyButton';
 import SearchBar1 from '../component/SearchBar1';
 import SearchBar2 from '../component/SearchBar2';
 
 const MainScreen = () => {
   return (
-    <View style = {styles.container}>
-      <View style = {styles.header}>
-      <Text style = {{
-         alignSelf: 'flex-start',
-         fontSize: 12,
-         color: '#ffffff',
-         marginBottom: 10,
-         fontFamily: 'GmarketSansTTFMedium',
-      }}>내 손안의 작은 경호원</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text
+          style={{
+            alignSelf: 'flex-start',
+            fontSize: 12,
+            color: '#ffffff',
+            marginBottom: 10,
+            fontFamily: 'GmarketSansTTFMedium',
+          }}>
+          내 손안의 작은 경호원
+        </Text>
         <View style={{flexDirection: 'row', alignSelf: 'flex-start'}}>
-        <Text style = {styles.title}>Police In My Pocket 안심귀가 서비스</Text>
-        <Image source={require('../../assets/imgs/police-car.png')}
-        style={{
-          width:30,
-          height: 30,
-          marginLeft: 10,
-        }}></Image>
+          <Text style={styles.title}>Police In My Pocket 안심귀가 서비스</Text>
+          <Image
+            source={require('../../assets/imgs/police-car.png')}
+            style={{
+              width: 30,
+              height: 30,
+              marginLeft: 10,
+            }}
+          />
         </View>
       </View>
-      <View style = {styles.content1}>
-        <SearchBar1/>
-        <SearchBar2/>
+      <View style={styles.content1}>
+        <SearchBar1 />
+        <SearchBar2 />
       </View>
-      <View style = {styles.content}>
-        <MapView 
-              style={{ width: '90%', height: '90%',  
-              borderRadius: 20, 
-              overflow: 'hidden',
-              borderWidth: 1, }} 
-              provider={PROVIDER_GOOGLE}
-              initialRegion={{ latitude: 37.601948, longitude: 127.041518, latitudeDelta: 0.008, longitudeDelta: 0.008, }} 
-            ></MapView>
+      <View style={styles.content}>
+        <MapView
+          style={{
+            width: '90%',
+            height: '90%',
+            borderRadius: 20,
+            overflow: 'hidden',
+            borderWidth: 1,
+          }}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.601948,
+            longitude: 127.041518,
+            latitudeDelta: 0.008,
+            longitudeDelta: 0.008,
+          }}
+        />
       </View>
       <View style={styles.footer}>
-        <Text style ={{color:'#FFFFFF', alignItems:'center', paddingBottom:5, fontFamily:'GmarketSansTTFMedium'}} >안심귀가를 시작하겠습니까?</Text>
+        <Text
+          style={{
+            color: '#FFFFFF',
+            alignItems: 'center',
+            paddingBottom: 5,
+            fontFamily: 'GmarketSansTTFMedium',
+          }}>
+          안심귀가를 시작하겠습니까?
+        </Text>
         <MyButton />
       </View>
     </View>
@@ -54,7 +75,7 @@ const styles = StyleSheet.create({
     padding: 8,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#6799FF'
+    backgroundColor: '#6799FF',
   },
   header: {
     width: '100%',
@@ -62,12 +83,11 @@ const styles = StyleSheet.create({
     //backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
-   
   },
   map: {
-    width: '100%', height: '100%',
-   
-  }, 
+    width: '100%',
+    height: '100%',
+  },
   content1: {
     width: '100%',
     height: '10%',
@@ -76,11 +96,11 @@ const styles = StyleSheet.create({
   },
   content: {
     width: '90%',
-    height:'60%',
+    height: '60%',
     //backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 20, 
+    borderRadius: 20,
     overflow: 'hidden',
     backgroundColor: 'white',
   },
@@ -97,8 +117,7 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     marginBottom: 10,
     fontFamily: 'GmarketSansTTFMedium',
-    
   },
 });
- 
+
 export default MainScreen;
--- Police-in-my-pocket-Frontend/src/screen/MenuScreen.js
+++ Police-in-my-pocket-Frontend/src/screen/MenuScreen.js
@@ -1,110 +1,122 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
-import Geolocation from 'react-native-geolocation-service'
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';	
+import Geolocation from 'react-native-geolocation-service';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import styled from 'styled-components';
-import {useNavigation} from '@react-navigation/native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const ViewContainerMap = styled.View`
   flex: 1;
-  justifyContent: center;
-  aliginItems: center;
-  backgroundColor: #709eff;
-`
+  justifycontent: center;
+  aliginitems: center;
+  backgroundcolor: #709eff;
+`;
 
 const ViewContainerButton = styled.View`
   flex: 1;
-  justifyContent: center;
-  aliginItems: center;
-  backgroundColor: black;
-`
+  justifycontent: center;
+  aliginitems: center;
+  backgroundcolor: #709eff;
+`;
 
 const styles = StyleSheet.create({
   button: {
-    alignItems: "center",
-    backgroundColor: "#FFFFFF",
-    padding: 10,
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    padding: 8,
     margin: 8,
-    flexDirection: 'row'
-  }
-
-})
-const MapExample = ({ navigation }) => {
+    flexDirection: 'row',
+    borderRadius: 20,
+  },
+});
+const MapExample = ({navigation}) => {
   const [longitude, setLongitude] = useState();
   const [latitude, setLatitude] = useState();
 
   useEffect(() => {
     Geolocation.getCurrentPosition(
-      (position) => {
-          console.log(position);
+      position => {
+        console.log(position);
 
-          const lo = JSON.stringify(position.coords.longitude);
-          const la = JSON.stringify(position.coords.latitude);
-          setLongitude(lo);
-          setLatitude(la);
+        const lo = JSON.stringify(position.coords.longitude);
+        const la = JSON.stringify(position.coords.latitude);
+        setLongitude(lo);
+        setLatitude(la);
       },
-      (error) => {
-          console.log(error.code, error.message);
+      error => {
+        console.log(error.code, error.message);
       },
       {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
     );
-  })
+  });
 
-    return (
-      <>
+  return (
+    <>
       <ViewContainerMap>
-          <MapView 
-            style={{ 
-              width: '80%', 
-              height: '80%',
-              position: 'absolute',
-              marginLeft: 43,
-             }} 
-            provider={PROVIDER_GOOGLE}
-            initialRegion={{ latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421, }} 
-          />
-        </ViewContainerMap>
-        <ViewContainerButton>
-          <TouchableOpacity style={styles.button}>
-          <Image source={require('../../assets/imgs/police-car.png')}
-        style={{
-          width:30,
-          height: 30,
-          marginLeft: 10,
-        }}></Image><Text>&nbsp;&nbsp;&nbsp;</Text>
-            <Text>긴급상황 발생! 인근 파출소 혹은 가족에게 신고하기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-          <Image source={require('../../assets/imgs/police-car.png')}
-        style={{
-          width:30,
-          height: 30,
-          marginLeft: 10,
-        }}></Image><Text>&nbsp;&nbsp;&nbsp;</Text>
-            <Text>안심 귀가 서비스 이용하기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-          <Image source={require('../../assets/imgs/police-car.png')}
-        style={{
-          width:30,
-          height: 30,
-          marginLeft: 10,
-        }}></Image><Text>&nbsp;&nbsp;&nbsp;</Text>
-            <Text>주요 연락처 등록하기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-          <Image source={require('../../assets/imgs/police-car.png')}
-        style={{
-          width:30,
-          height: 30,
-          marginLeft: 10,
-        }}></Image><Text>&nbsp;&nbsp;&nbsp;</Text>
-            <Text>실시간 위험 지역 / 치안 시설 정보 확인하기</Text>
-          </TouchableOpacity>
-          </ViewContainerButton>
-      </>
-    );
+        <MapView
+          style={{
+            width: '80%',
+            height: '80%',
+            position: 'absolute',
+            marginLeft: 43,
+          }}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </ViewContainerMap>
+      <ViewContainerButton>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('../../assets/imgs/police-car.png')}
+            style={{
+              width: 30,
+              height: 30,
+              marginLeft: 10,
+            }}></Image>
+          <Text>&nbsp;&nbsp;&nbsp;</Text>
+          <Text>긴급상황 발생! 인근 파출소 혹은 가족에게 신고하기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('../../assets/imgs/police-car.png')}
+            style={{
+              width: 30,
+              height: 30,
+              marginLeft: 10,
+            }}></Image>
+          <Text>&nbsp;&nbsp;&nbsp;</Text>
+          <Text>안심 귀가 서비스 이용하기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('../../assets/imgs/police-car.png')}
+            style={{
+              width: 30,
+              height: 30,
+              marginLeft: 10,
+            }}></Image>
+          <Text>&nbsp;&nbsp;&nbsp;</Text>
+          <Text>주요 연락처 등록하기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('../../assets/imgs/police-car.png')}
+            style={{
+              width: 30,
+              height: 30,
+              marginLeft: 10,
+            }}></Image>
+          <Text>&nbsp;&nbsp;&nbsp;</Text>
+          <Text>실시간 위험 지역 / 치안 시설 정보 확인하기</Text>
+        </TouchableOpacity>
+      </ViewContainerButton>
+    </>
+  );
 };
 
 export default MapExample;
